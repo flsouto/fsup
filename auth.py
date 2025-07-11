bff2e3d9905c2d3b0aa6a4155b9be0af4c6d7149
flsouto <fabiolimasouto@gmail.com>
2025-07-11 14:54:31 -0300

    updates

--- auth.py
+++ auth.py
@@ -8,4 +8,4 @@ client_id = getenv('client_id')
 url = f'https://freesound.org/apiv2/oauth2/authorize/?client_id={client_id}&response_type=code&state=x'
 print(url)
 
-system(f"firefox '{url}'")
+system(f"brave-browser '{url}' || firefox '{url}'")
