3024eaa8f19059a44092fa5d73ef5a556d0bf8ef
flsouto <fabiolimasouto@gmail.com>
2025-08-19 10:03:23 -0300

    updates

--- upnext.py
+++ upnext.py
@@ -120,7 +120,7 @@ files = {
 data = {
     "name": title,
     "description": desc,
-    "license" : "Creative",
+    "license" : "Creative Commons",
     "tags": tags,
     "pack": pack,
     "bst_category": bst_category
